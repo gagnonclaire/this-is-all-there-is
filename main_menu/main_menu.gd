a593153d25c4e374ea368624ae87b85b717f4b29
gagnonblake <gagnon.blake@gmail.com>
2024-03-11 11:02:48 -0500

    Clean up themes and improve join game flow

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,19 +1,31 @@
 extends Node
 
+const MENU_SCENE_1: PackedScene = preload("res://main_menu/menu_scene_1.tscn")
+
 @onready var main_node: Node = get_parent()
-@onready var address_entry: LineEdit = $CanvasLayer/MarginContainer/VBoxContainer/HBoxContainer/AddressEntry
+
+@onready var address_entry: LineEdit = $CanvasLayer/JoinGameMenu/Panel/VBoxContainer/MarginContainer/AddressEntry
+@onready var join_game_menu: Control = $CanvasLayer/JoinGameMenu
+
+func _ready():
+	# Load a random menu scene
+	add_child(MENU_SCENE_1.instantiate())
 
 func _on_new_game_button_pressed():
 	main_node.is_host = true
 	main_node.load_world()
 
 func _on_load_game_button_pressed():
-	main_node.is_host = true
-	main_node.load_world()
+	pass
 
 func _on_join_game_button_pressed():
+	join_game_menu.show()
+
+func _on_join_button_pressed():
 	main_node.is_host = false
 	main_node.join_address = address_entry.text
+
+	print("Joining game at ", main_node.join_address, ":9999")
 	main_node.load_world()
 
 func _on_exit_button_pressed():
