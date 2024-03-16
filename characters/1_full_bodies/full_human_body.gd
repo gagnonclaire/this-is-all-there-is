5996d8c62ac1f88ffc8c84d3a06e8f2a0d931e93
gagnonblake <gagnon.blake@gmail.com>
2024-03-15 23:37:33 -0500

    Add hand and foot models

--- characters/1_full_bodies/full_human_body.gd
+++ characters/1_full_bodies/full_human_body.gd
@@ -7,19 +7,27 @@
 
 extends Node3D
 
-# Preload all party type meshes
+# Preload all part type meshes
 const BODY_ARRAY: Array[PackedScene] = \
 [ preload("res://characters/2_parts_bodies/human_body_1.tscn"), \
 preload("res://characters/2_parts_bodies/human_body_2.tscn")]
 const HEAD_ARRAY: Array[PackedScene] = \
 [ preload("res://characters/2_parts_heads/human_head_1.tscn")]
+const HAND_ARRAY: Array[PackedScene] = \
+[ preload("res://characters/2_parts_hands/human_hand_1.tscn")]
+const FOOT_ARRAY: Array[PackedScene] = \
+[ preload("res://characters/2_parts_feet/human_foot_1.tscn")]
 
 # Synced variables
 @export var body_type: int = 0
 @export var head_type: int = 0
+@export var hand_type: int = 0
+@export var foot_type: int = 0
 @export var head_color: Color = Color(1, 1, 1, 1)
 @export var body_top_color: Color = Color(1, 1, 1, 1)
 @export var body_bottom_color: Color = Color(1, 1, 1, 1)
+@export var hand_color: Color = Color(1, 1, 1, 1)
+@export var foot_color: Color = Color(1, 1, 1, 1)
 
 # Exposed nodes
 @onready var skeleton: Skeleton3D = $Skeleton3D
@@ -27,23 +35,31 @@ const HEAD_ARRAY: Array[PackedScene] = \
 @onready var head_pivot: Node3D = $Skeleton3D/CoreBone/HeadPivot
 @onready var camera_pivot: Node3D = $Skeleton3D/CoreBone/CameraPivot
 @onready var body_pivot: Node3D = $Skeleton3D/CoreBone/BodyPivot
-@onready var left_hand: Node3D = $Skeleton3D/CoreBone/LeftHandPivot
-@onready var right_hand: Node3D = $Skeleton3D/CoreBone/RightHandPivot
-@onready var left_foot: Node3D = $Skeleton3D/CoreBone/LeftFootPivot
-@onready var right_foot: Node3D = $Skeleton3D/CoreBone/RightFootPivot
+@onready var left_hand_pivot: Node3D = $Skeleton3D/CoreBone/LeftHandPivot
+@onready var right_hand_pivot: Node3D = $Skeleton3D/CoreBone/RightHandPivot
+@onready var left_foot_pivot: Node3D = $Skeleton3D/CoreBone/LeftFootPivot
+@onready var right_foot_pivot: Node3D = $Skeleton3D/CoreBone/RightFootPivot
 
 # Part scenes
 var body_node: Node3D = null
 var head_node: Node3D = null
+var left_hand_node: Node3D = null
+var right_hand_node: Node3D = null
+var left_foot_node: Node3D = null
+var right_foot_node: Node3D = null
 
 func _ready() -> void:
 	if is_multiplayer_authority():
 		body_type = randi_range(0, BODY_ARRAY.size() - 1)
 		head_type = randi_range(0, HEAD_ARRAY.size() - 1)
+		hand_type = randi_range(0, HAND_ARRAY.size() - 1)
+		foot_type = randi_range(0, FOOT_ARRAY.size() - 1)
 
 		head_color = Color(randf(), randf(), randf(), 1)
 		body_top_color = Color(randf(), randf(), randf(), 1)
 		body_bottom_color = Color(randf(), randf(), randf(), 1)
+		hand_color = Color(randf(), randf(), randf(), 1)
+		foot_color = Color(randf(), randf(), randf(), 1)
 
 #TODO Replace this timer hack with an actual way to load colors after sync
 #TODO They are syncing just fine but never updating after that
@@ -53,26 +69,52 @@ func _on_body_update_timer_timeout():
 #TODO Rewrite this so its not just a bunch of duplicate code
 func update_body() -> void:
 	if !body_node:
-		body_node = BODY_ARRAY[body_type].instantiate()
-		body_pivot.add_child(body_node)
-
+		# Head
 		head_node = HEAD_ARRAY[head_type].instantiate()
 		head_pivot.add_child(head_node)
-
 		var head_material = StandardMaterial3D.new()
 		head_material.set_albedo(head_color)
-
 		var head_mesh: MeshInstance3D = head_pivot.get_child(0).get_child(0)
 		head_mesh.set_surface_override_material(0, head_material)
 
+		# Body
+		body_node = BODY_ARRAY[body_type].instantiate()
+		body_pivot.add_child(body_node)
 		var body_top_material = StandardMaterial3D.new()
 		var body_bottom_material = StandardMaterial3D.new()
 		body_top_material.set_albedo(body_top_color)
 		body_bottom_material.set_albedo(body_bottom_color)
-
 		var body_mesh: MeshInstance3D = body_pivot.get_child(0).get_child(0)
 		body_mesh.set_surface_override_material(0, body_top_material)
 		body_mesh.set_surface_override_material(1, body_bottom_material)
+
+		# Hands
+		left_hand_node = HAND_ARRAY[hand_type].instantiate()
+		right_hand_node = HAND_ARRAY[hand_type].instantiate()
+		left_hand_pivot.add_child(left_hand_node)
+		right_hand_pivot.add_child(right_hand_node)
+		var hand_material = StandardMaterial3D.new()
+		hand_material.set_albedo(hand_color)
+		var left_hand_mesh: MeshInstance3D = left_hand_pivot.get_child(0).get_child(0)
+		var right_hand_mesh: MeshInstance3D = right_hand_pivot.get_child(0).get_child(0)
+		left_hand_mesh.set_surface_override_material(0, hand_material)
+		right_hand_mesh.set_surface_override_material(0, hand_material)
+
+		# Feet
+		left_foot_node = HAND_ARRAY[foot_type].instantiate()
+		right_foot_node = HAND_ARRAY[foot_type].instantiate()
+		left_foot_pivot.add_child(left_foot_node)
+		right_foot_pivot.add_child(right_foot_node)
+		var foot_material = StandardMaterial3D.new()
+		foot_material.set_albedo(foot_color)
+		var left_foot_mesh: MeshInstance3D = left_foot_pivot.get_child(0).get_child(0)
+		var right_foot_mesh: MeshInstance3D = right_foot_pivot.get_child(0).get_child(0)
+		left_foot_mesh.set_surface_override_material(0, foot_material)
+		right_foot_mesh.set_surface_override_material(0, foot_material)
+
+
+
+
 
 
 
