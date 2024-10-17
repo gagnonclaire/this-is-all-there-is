83690395c0eb10d4879334e379e475d6c6aa24e3
gagnonclaire <gagnon.blake@gmail.com>
2024-10-16 20:29:07 -0500

    Add map saving and loading

--- addons/board_creator_plugin/board_creator_inspector.gd
+++ addons/board_creator_plugin/board_creator_inspector.gd
@@ -22,32 +22,12 @@ func _parse_begin(object):
 	btn_delete.pressed.connect(object.delete)
 	add_custom_control(btn_delete)
 
-	var btn_growArea = Button.new()
-	btn_growArea.set_text("Grow Area")
-	btn_growArea.pressed.connect(object.GrowArea)
-	add_custom_control(btn_growArea)
-
-	var btn_shrinkArea = Button.new()
-	btn_shrinkArea.set_text("Shrink Area")
-	btn_shrinkArea.pressed.connect(object.ShrinkArea)
-	add_custom_control(btn_shrinkArea)
-
 	var btn_save = Button.new()
 	btn_save.set_text("Save")
-	btn_save.pressed.connect(object.Save)
+	btn_save.pressed.connect(object.save_map)
 	add_custom_control(btn_save)
 
-	var btn_load_tile = Button.new()
-	btn_load_tile.set_text("Load Tile")
-	btn_load_tile.pressed.connect(object.load_tile)
-	add_custom_control(btn_load_tile)
-
-	var btn_saveJSON = Button.new()
-	btn_saveJSON.set_text("Save JSON")
-	btn_saveJSON.pressed.connect(object.SaveJSON)
-	add_custom_control(btn_saveJSON)
-
-	var btn_loadJSON = Button.new()
-	btn_loadJSON.set_text("Load JSON")
-	btn_loadJSON.pressed.connect(object.LoadJSON)
-	add_custom_control(btn_loadJSON)
+	var btn_load = Button.new()
+	btn_load.set_text("Load")
+	btn_load.pressed.connect(object.load_map)
+	add_custom_control(btn_load)
